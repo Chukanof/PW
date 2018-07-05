d518e5133145940d624679f805d560dd8f9443a8
Denis Chukanov <Chudzzi@gmail.com>
2018-07-05 14:04:39 +0300

    webpack config improves in prod build stage

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -4,14 +4,23 @@ const webpack = require("webpack");
 const config = {
   mode: "production",
   devtool: "none",
+
   plugins: [
-    new UglifyJsWebpackPlugin({
-      sourceMap: false
-    }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     })
-  ]
+  ],
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsWebpackPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false
+      })
+    ]
+  }
 };
 
 module.exports = config;
